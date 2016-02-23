e7f09cd9361777de108d64fe23545af49d499206
Steve Karmeinsky <steve@gbnet.net>
2016-02-23 11:07:51 +0000

    Now works on all platforms and sized accordingly
    Moved stuff around and resized

--- src/iss_windows.c
+++ src/iss_windows.c
@@ -4,9 +4,10 @@
 
 // BEGIN AUTO-GENERATED UI CODE; DO NOT MODIFY
 static Window *s_window;
-static GFont s_res_gothic_18_bold;
+static GFont s_res_gothic_24;
 static GFont s_res_gothic_28;
 static GFont s_res_gothic_28_bold;
+static GFont s_res_gothic_24_bold;
 static TextLayer *s_Up;
 static TextLayer *s_Down;
 static TextLayer *s_HomeAway;
@@ -25,83 +26,84 @@ static void initialise_ui(void) {
     window_set_fullscreen(s_window, 0);
   #endif
   
-  s_res_gothic_18_bold = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
+  s_res_gothic_24 = fonts_get_system_font(FONT_KEY_GOTHIC_24);
   s_res_gothic_28 = fonts_get_system_font(FONT_KEY_GOTHIC_28);
   s_res_gothic_28_bold = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
+  s_res_gothic_24_bold = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
   // s_Up
-  s_Up = text_layer_create(GRect(117, 6, 20, 20));
+  s_Up = text_layer_create(GRect(122, 15, 20, 24));
   text_layer_set_text(s_Up, "+");
   text_layer_set_text_alignment(s_Up, GTextAlignmentCenter);
-  text_layer_set_font(s_Up, s_res_gothic_18_bold);
+  text_layer_set_font(s_Up, s_res_gothic_24_bold);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_Up);
   
   // s_Down
-  s_Down = text_layer_create(GRect(117, 124, 20, 20));
+  s_Down = text_layer_create(GRect(122, 114, 20, 24));
   text_layer_set_text(s_Down, "-");
   text_layer_set_text_alignment(s_Down, GTextAlignmentCenter);
-  text_layer_set_font(s_Down, s_res_gothic_18_bold);
+  text_layer_set_font(s_Down, s_res_gothic_24_bold);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_Down);
   
   // s_HomeAway
-  s_HomeAway = text_layer_create(GRect(3, 6, 17, 26));
+  s_HomeAway = text_layer_create(GRect(83, 120, 19, 27));
   text_layer_set_text(s_HomeAway, "A");
   text_layer_set_text_alignment(s_HomeAway, GTextAlignmentCenter);
-  text_layer_set_font(s_HomeAway, s_res_gothic_18_bold);
+  text_layer_set_font(s_HomeAway, s_res_gothic_24_bold);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_HomeAway);
   
   // s_Status
-  s_Status = text_layer_create(GRect(69, 56, 73, 31));
+  s_Status = text_layer_create(GRect(67, 65, 75, 31));
   text_layer_set_text(s_Status, "Start");
-  text_layer_set_text_alignment(s_Status, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_Status, GTextAlignmentRight);
   text_layer_set_font(s_Status, s_res_gothic_28);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_Status);
   
   // s_HomeScore
-  s_HomeScore = text_layer_create(GRect(13, 47, 55, 32));
+  s_HomeScore = text_layer_create(GRect(22, 47, 49, 32));
   text_layer_set_text(s_HomeScore, " ---");
-  text_layer_set_text_alignment(s_HomeScore, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_HomeScore, GTextAlignmentRight);
   text_layer_set_font(s_HomeScore, s_res_gothic_28_bold);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_HomeScore);
   
   // s_AwayScore
-  s_AwayScore = text_layer_create(GRect(13, 82, 55, 32));
+  s_AwayScore = text_layer_create(GRect(22, 81, 49, 32));
   text_layer_set_text(s_AwayScore, " ---");
-  text_layer_set_text_alignment(s_AwayScore, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_AwayScore, GTextAlignmentRight);
   text_layer_set_font(s_AwayScore, s_res_gothic_28_bold);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_AwayScore);
   
   // s_TimeLeft
-  s_TimeLeft = text_layer_create(GRect(26, 5, 87, 37));
+  s_TimeLeft = text_layer_create(GRect(33, 7, 95, 37));
   text_layer_set_text(s_TimeLeft, "--:--.-");
   text_layer_set_text_alignment(s_TimeLeft, GTextAlignmentCenter);
   text_layer_set_font(s_TimeLeft, s_res_gothic_28_bold);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_TimeLeft);
   
   // s_Innings
-  s_Innings = text_layer_create(GRect(33, 119, 14, 26));
+  s_Innings = text_layer_create(GRect(60, 120, 14, 26));
   text_layer_set_text(s_Innings, "-");
   text_layer_set_text_alignment(s_Innings, GTextAlignmentCenter);
-  text_layer_set_font(s_Innings, s_res_gothic_18_bold);
+  text_layer_set_font(s_Innings, s_res_gothic_24_bold);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_Innings);
   
   // s_Home_const
-  s_Home_const = text_layer_create(GRect(0, 52, 12, 20));
+  s_Home_const = text_layer_create(GRect(11, 52, 14, 28));
   text_layer_set_text(s_Home_const, "H");
   text_layer_set_text_alignment(s_Home_const, GTextAlignmentCenter);
-  text_layer_set_font(s_Home_const, s_res_gothic_18_bold);
+  text_layer_set_font(s_Home_const, s_res_gothic_24);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_Home_const);
   
   // s_Away_const
-  s_Away_const = text_layer_create(GRect(0, 87, 12, 20));
+  s_Away_const = text_layer_create(GRect(12, 84, 14, 28));
   text_layer_set_text(s_Away_const, "A");
   text_layer_set_text_alignment(s_Away_const, GTextAlignmentCenter);
-  text_layer_set_font(s_Away_const, s_res_gothic_18_bold);
+  text_layer_set_font(s_Away_const, s_res_gothic_24);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_Away_const);
   
   // s_Inn_const
-  s_Inn_const = text_layer_create(GRect(4, 119, 27, 26));
+  s_Inn_const = text_layer_create(GRect(25, 120, 30, 26));
   text_layer_set_text(s_Inn_const, "Inn:");
-  text_layer_set_font(s_Inn_const, s_res_gothic_18_bold);
+  text_layer_set_font(s_Inn_const, s_res_gothic_24);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_Inn_const);
 }
 
